bc410ec04b7b278c9db80c3e6f8fdea87a8d1a36
Roch Devost <roch.devost@gmail.com>
2017-04-20 19:32:59 -0400

    refactor with better json stringification (#8)

--- src/recorder.js
+++ src/recorder.js
@@ -1,26 +1,26 @@
 'use strict'
 
 const request = require('superagent')
+const bignumJSON = require('json-bignum')
 
 class DatadogRecorder {
   record (span) {
     const tracer = span.tracer()
     const spanContext = span.context()
 
-    // TODO: find a better way to handle int64
-    const body = JSON.stringify([[{
-      trace_id: spanContext.traceId.toString(),
-      span_id: spanContext.spanId.toString(),
-      parent_id: span._parentId ? span._parentId.toString() : null,
+    const body = bignumJSON.stringify([[{
+      trace_id: new bignumJSON.BigNumber(spanContext.traceId.toString()),
+      span_id: new bignumJSON.BigNumber(spanContext.spanId.toString()),
+      parent_id: span._parentId ? new bignumJSON.BigNumber(span._parentId.toString()) : null,
       name: span._operationName,
       resource: span._tags.resource,
       service: tracer._service,
       type: span._tags.type,
-      error: span._tags.error ? 1 : 0,
+      error: +!!span._tags.error,
       meta: span._tags,
       start: Math.round(span._startTime * 1e6),
       duration: Math.round(span._duration * 1e6)
-    }]]).replace(/"(trace_id|span_id|parent_id)":"(\d+)"/g, '"$1":$2')
+    }]])
 
     return request
       .put(`${tracer._endpoint}/v0.3/traces`)
