ae976d6c5d6641cbbe96e8a3147a723474328b9e
rochdev <roch.devost@gmail.com>
2017-04-23 12:04:37 -0400

    optimize generating IDs

--- src/platform.js
+++ src/platform.js
@@ -1,11 +1,26 @@
 'use strict'
 
 const now = require('performance-now')
+const Long = require('long')
 const loadNs = now()
 const loadMs = Date.now()
 
 module.exports = {
   now () {
     return Math.round((loadMs + now() - loadNs) * 100000) / 100000
+  },
+
+  id () {
+    return new Long(random(), random(), true)
   }
+}
+
+function random () {
+  let number = 0
+
+  for (let i = 0; i < 4; i++) {
+    number += Math.floor(Math.random() * 255) << (i * 8)
+  }
+
+  return number
 }
--- test/platform.spec.js
+++ test/platform.spec.js
@@ -8,17 +8,28 @@ describe('Platform', () => {
 
   beforeEach(() => {
     sinon.stub(Date, 'now').returns(1000000000)
+    sinon.stub(Math, 'random')
     now = sinon.stub().returns(100.11111)
     platform = proxyquire('../src/platform', { 'performance-now': now })
   })
 
   afterEach(() => {
     Date.now.restore()
+    Math.random.restore()
   })
 
   it('should return the current time in milliseconds with high resolution', () => {
     now.returns(600.33333)
 
     expect(platform.now()).to.equal(1000000500.22222)
+  })
+
+  it('should return a random 64bit ID', () => {
+    Math.random.onCall(0).returns(0)
+    Math.random.onCall(1).returns(1)
+    Math.random.onCall(2).returns(0)
+    Math.random.onCall(3).returns(0)
+
+    expect(platform.id().toString()).to.equal(String(255 << 8))
   })
 })
