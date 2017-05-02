331ef1517511cd5ed0a34692d11d276ffe4c7a80
Roch Devost <roch.devost@gmail.com>
2017-05-02 17:44:52 -0400

    refactor platform to prepare for browserify (#21)

--- test/api.spec.js
+++ test/api.spec.js
@@ -1,6 +1,6 @@
 'use strict'
 
 const apiCompatibilityChecks = require('opentracing/lib/test/api_compatibility').default
-const DatadogTracer = require('../src/tracer')
+const DatadogTracer = require('../src')
 
 apiCompatibilityChecks(() => new DatadogTracer({ service: 'test' }))
